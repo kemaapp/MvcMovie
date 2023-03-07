a7c9e5173146e4c5a67053cc5bbd9ca908608fbd
kemaapp <kma.uni.app@gmail.com>
2023-03-06 17:56:44 -0800

    Add project files.

---
+++ MvcMovie/Pages/Product.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMovie.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private readonly ILogger<ProductModel> _logger;
+
+        public ProductModel(ILogger<ProductModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
